df356e2ba7ca09c52e0576f328d62192ec7d81a9
bri <284789+b-@users.noreply.github.com>
2023-12-18 02:23:28 -0500

    conditional providers, fix actions

--- inc/zones/ibeep.com/00-ibeep.com.js
+++ inc/zones/ibeep.com/00-ibeep.com.js
@@ -1,7 +1,24 @@
+// See https://docs.dnscontrol.org/advanced-features/cli-variables
+CLI_DEFAULTS({
+  "bind": "false",
+  "axfr": "false"
+});
+
+
 D("ibeep.com", REG_CHANGEME,
   DnsProvider(DSP_CLOUDFLARE),
-  DnsProvider(DSP_BIND),
-  //DnsProvider(AXFRDDNS),
+  // DnsProvider(DSP_BIND),
+  // DnsProvider(AXFRDDNS),
   DefaultTTL(1)
 )
-  //require_glob("./contents/")
+if (bind=="true") {
+  D_EXTEND("ibeep.com",
+    DnsProvider(DSP_BIND)
+  )
+}
+
+if (axfr=="true") {
+  D_EXTEND("ibeep.com",
+    DnsProvider(AXFRDDNS)
+  )
+}
--- inc/zones/ibeep.com/contents/cf-apex-record.js
+++ inc/zones/ibeep.com/contents/cf-apex-record.js
@@ -2,5 +2,13 @@ D_EXTEND("ibeep.com",
 // NOTE: CNAME at apex
   // does not work with BIND
 //  ALIAS("@", "apex-loadbalancer.netlify.com.")
-  IGNORE("@","CNAME")
 )
+if (bind != "true") {
+  D_EXTEND("ibeep.com",
+    ALIAS("@", "apex-loadbalancer.netlify.com.")
+  )
+} else {
+  D_EXTEND("ibeep.com",
+    IGNORE("@","CNAME")
+  )
+}
