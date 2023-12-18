5263b3c5c1d3123d00d932fea021c53808672d77
bri <284789+b-@users.noreply.github.com>
2023-12-18 18:19:13 -0500

    Clean up some old and testing records

--- inc/zones/ibeep.com/contents/ddns.inc.js
+++ inc/zones/ibeep.com/contents/ddns.inc.js
@@ -2,10 +2,6 @@ D_EXTEND("ibeep.com"
 	, IGNORE("*-ddns","A") // managed by ddns updater tools
 	, IGNORE("tdns1","AAAA") // managed by ddns updater tools
 
-	, CNAME("test-subdomain", "home-ddns.ibeep.com.")
-
 	, CNAME("home", "home-ddns.ibeep.com.")
 	, CNAME("mediabox.home", "home-ddns.ibeep.com.", TTL(60))
-	, CNAME("opnsense.home", "home-ddns.ibeep.com.", TTL(60))
-	, CNAME("pfsense.home", "home-ddns.ibeep.com.", TTL(3200))
 )
