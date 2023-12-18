2394b76253508a5591636acc2f36fa0038ed3bb3
bri <284789+b-@users.noreply.github.com>
2023-12-18 02:38:16 -0500

    fix zone

--- inc/zones/ibeep.com/contents/cf-apex-record.js
+++ inc/zones/ibeep.com/contents/cf-apex-record.js
@@ -1,8 +1,3 @@
-D_EXTEND("ibeep.com",
-// NOTE: CNAME at apex
-  // does not work with BIND
-//  ALIAS("@", "apex-loadbalancer.netlify.com.")
-)
 if (bind != "true") {
   D_EXTEND("ibeep.com",
     ALIAS("@", "apex-loadbalancer.netlify.com.")
