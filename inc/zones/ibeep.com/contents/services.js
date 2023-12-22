35aa26a9cb252ccd8fc54b1bc4184813402000b8
bri <284789+b-@users.noreply.github.com>
2023-12-21 20:22:27 -0500

    typo

--- inc/zones/ibeep.com/contents/services.js
+++ inc/zones/ibeep.com/contents/services.js
@@ -1,4 +1,4 @@
 D_EXTEND("ibeep.com"
-  , CNAME("c.int", "coolify.v31.internal.ibeep.com.")
-  , CNAME("*.c.int", "coolify.v31.internal.ibeep.com.")
+  , CNAME("c.int",   "coolify-1.v31.internal.ibeep.com.")
+  , CNAME("*.c.int", "coolify-1.v31.internal.ibeep.com.")
 )
