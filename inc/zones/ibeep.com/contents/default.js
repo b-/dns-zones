b0cad88720b58a6a14e98d6bc2cc965b1ec5f9c3
bri <284789+b-@users.noreply.github.com>
2025-02-21 22:53:53 -0500

    .

--- inc/zones/ibeep.com/contents/default.js
+++ inc/zones/ibeep.com/contents/default.js
@@ -44,14 +44,7 @@ D_EXTEND("ibeep.com"
 
 	////////////////
 	// verification things
-	, CNAME("_domainconnect", "connect.domains.google.com.")
-	, IGNORE("_acme-challenge", "TXT")
-	, IGNORE("_acme-challenge.**", "TXT")
-	, TXT("_atproto", "did=did:plc:fvgyuzwvn5jac6z4dedztdzx")
-	, TXT("@", "oci-domain-verification=5sMTB8TMCj01LpqaioMRRslg9wQD0RFwUzqxHlwCIySv2")
 	, TXT("@", "keybase-site-verification=ETl8o2A9sJFg3seiGRyUdGfP6EDURyJYy14zT-gTgzo")
-	, TXT("e", "MS=ms34099336", TTL(3600))
-	, TXT("oci-domain-verification", "sMTB8TMCj01LpqaioMRRslg9wQD0RFwUzqxHlwCIySv2")
 
 	/////////////////
 	// subdomain ns delegations
---
+++ inc/zones/ibeep.com/contents/verifications/acme-challenge.inc.js
@@ -0,0 +1,4 @@
+D_EXTEND("ibeep.com"
+	, IGNORE("_acme-challenge", "TXT")
+	, IGNORE("_acme-challenge.**", "TXT")
+)
