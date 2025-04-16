a4b66b84fb1f9f37791faeb1ec089d17327f2fbd
bri <284789+b-@users.noreply.github.com>
2025-04-16 15:28:56 -0400

    add mediaboxlite (#46)

---
+++ inc/zones/ibeep.com/contents/hosts/homelab/mediaboxlite.inc.js
@@ -0,0 +1,9 @@
+D_EXTEND("ibeep.com"
+	///////////////
+	// mediabox lite records
+	, CNAME("mediaboxlite", "mediaboxlite.lan.ibeep.com.")
+	, CNAME("mediaboxlite.home", "home-ddns.ibeep.com.", TTL(60))
+	, A("mediaboxlite.lan", "192.168.1.80", TTL(60))
+	, A("mediaboxlite.ts", "100.127.24.51", TTL(60))
+	, CNAME("*.mediaboxlite", "mediaboxlite.ibeep.com.")
+)
