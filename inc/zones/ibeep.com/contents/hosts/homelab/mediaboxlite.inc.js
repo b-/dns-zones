f28bb1e5bc764f78b8e24bc5cc98a258779b7ba5
bri <284789+b-@users.noreply.github.com>
2025-04-17 23:55:38 -0400

    update mediaboxlite tailscale ip

--- inc/zones/ibeep.com/contents/hosts/homelab/mediaboxlite.inc.js
+++ inc/zones/ibeep.com/contents/hosts/homelab/mediaboxlite.inc.js
@@ -4,7 +4,7 @@ D_EXTEND("ibeep.com"
 	, CNAME("mediaboxlite", "mediaboxlite.lan.ibeep.com.")
 	, CNAME("mediaboxlite.home", "home-ddns.ibeep.com.", TTL(60))
 	, A("mediaboxlite.lan", "192.168.1.80", TTL(60))
-	, A("mediaboxlite.ts", "100.127.24.51", TTL(60))
+	, A("mediaboxlite.ts", "100.97.198.120", TTL(60))
 	, CNAME("*.mediaboxlite", "mediaboxlite.ibeep.com.")
 	, CNAME("*.mediaboxlite.lan", "mediaboxlite.lan.ibeep.com.")
 	, CNAME("*.mediaboxlite.ts", "mediaboxlite.ts.ibeep.com.")
