34af5fca2e0668447ab610cc269123e263f95328
bri <284789+b-@users.noreply.github.com>
2025-04-18 13:21:24 -0400

    add subdomain wildcard for *.mediaboxlite.home

--- inc/zones/ibeep.com/contents/hosts/homelab/mediaboxlite.inc.js
+++ inc/zones/ibeep.com/contents/hosts/homelab/mediaboxlite.inc.js
@@ -6,6 +6,7 @@ D_EXTEND("ibeep.com"
 	, A("mediaboxlite.lan", "192.168.1.80", TTL(60))
 	, A("mediaboxlite.ts", "100.97.198.120", TTL(60))
 	, CNAME("*.mediaboxlite", "mediaboxlite.ibeep.com.")
+	, CNAME("*.mediaboxlite.home", "home-ddns.ibeep.com.", TTL(60))
 	, CNAME("*.mediaboxlite.lan", "mediaboxlite.lan.ibeep.com.")
 	, CNAME("*.mediaboxlite.ts", "mediaboxlite.ts.ibeep.com.")
 )
