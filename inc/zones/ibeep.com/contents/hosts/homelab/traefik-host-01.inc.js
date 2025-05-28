058af4ab1997dd87fb6bd29c45dad289889e1359
bri <284789+b-@users.noreply.github.com>
2025-05-28 00:05:20 -0400

    update traefik-host-01.ts ip

--- inc/zones/ibeep.com/contents/hosts/homelab/traefik-host-01.inc.js
+++ inc/zones/ibeep.com/contents/hosts/homelab/traefik-host-01.inc.js
@@ -4,7 +4,7 @@ D_EXTEND("ibeep.com"
 	, CNAME("traefik-host-01", "traefik-host-01.lan.ibeep.com.")
 	, CNAME("traefik-host-01.home", "home-ddns.ibeep.com.", TTL(60))
 	, A("traefik-host-01.lan", "192.168.1.48", TTL(60))
-	, A("traefik-host-01.ts", "100.67.207.109", TTL(60))
+	, A("traefik-host-01.ts", "100.71.130.29", TTL(60))
 	, CNAME("*.traefik-host-01", "traefik-host-01.ibeep.com.")
 	, CNAME("*.traefik-host-01.home", "home-ddns.ibeep.com.", TTL(60))
 	, CNAME("*.traefik-host-01.lan", "traefik-host-01.lan.ibeep.com.")
