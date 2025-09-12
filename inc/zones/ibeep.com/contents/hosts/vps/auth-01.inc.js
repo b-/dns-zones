f920cd2e7b8706c53475cb320c14cf8ce319651c
bri <284789+b-@users.noreply.github.com>
2025-09-11 22:54:31 -0400

    add auth-01, update auth.ibeep.com url

---
+++ inc/zones/ibeep.com/contents/hosts/vps/auth-01.inc.js
@@ -0,0 +1,3 @@
+D_EXTEND("ibeep.com"
+	, A("auth-01", "129.213.95.252") // oracle free vps
+)
--- inc/zones/ibeep.com/contents/services/authentik.inc.js
+++ inc/zones/ibeep.com/contents/services/authentik.inc.js
@@ -1,3 +1,3 @@
 D_EXTEND("ibeep.com"
-	, CNAME("auth", "mediabox.ibeep.com.")
+	, CNAME("auth", "auth-01.ibeep.com.")
 )
